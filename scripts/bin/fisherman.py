00a4b2acb5762edf740e074a14effa99e23ddd0e
IceGreb <nikoverg10@gmail.com>
2023-09-08 09:38:41 +0300

    fixed splitFasta + fisherman.py

--- scripts/bin/fisherman.py
+++ scripts/bin/fisherman.py
@@ -3,33 +3,39 @@ from Bio import SeqIO
 import os
 import glob
 import re
+import argparse
 import pandas as pd
 Nattokinases = ["IPR000209","IPR010259","IPR023827","IPR022398","IPR023828","IPR015500","IPR036852"]
 Feruloyl_esterases = ["IPR011118","IPR010126","IPR002921","IPR043595","IPR029058"]
 Petases_Pet_hydrolases = ["IPR041127"]
 Cocaine_esterases = ["IPR002018","IPR000383","IPR013736","IPR005674"]
-path = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/interproscan_results"
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--input", help = "Input directory containing tsv files to fish")
+parser.add_argument("-o","--output", help = "Output file to write results")
+args = parser.parse_args()
+path = args.input
 os.chdir(path)
-def read_tsv_file(file_path):
-    with open (file_path, 'r') as f:
+
+def read_tsv_file(file_path, output_file):
+    with open (file_path, 'r') as f, open(output_file,'a') as out_f:
         domain = f.read().strip()
         queries = (re.findall("IPR[0-9]+",domain),file)
         #print(queries)
         for query in queries:
             for id in query:
                 if id in Nattokinases:
-                    print("Found matching Nattokinases domain:\t",id,"\tin tsv file:\t",file)
+                    out_f.write(f"Found matching Nattokinases domain:\t{id}\tin tsv file:\t{file}\n")
                 elif id in Feruloyl_esterases:
-                    print("Found matching Feruloyl_esterases domain:\t",id,"\tin tsv file:\t",file)
+                    out_f.write(f"Found matching Feruloyl_esterases domain:\t{id}\tin tsv file:\t{file}\n")
                 elif id in Petases_Pet_hydrolases:
-                    print("Found matching Petases_Pet_hydrolases domain:\t",id,"\tin tsv file:\t",file)
+                    out_f.write(f"Found matching Petases_Pet_hydrolases domain:\t{id}\tin tsv file:\t{file}\n")                
                 elif id in Cocaine_esterases:
-                    print("Found matching Cocaine_esterases domain:\t",id,"\tin tsv file:\t",file)
+                    out_f.write(f"Found matching Cocaine_esterases domain:\t{id}\tin tsv file:\t{file}\n")
                 else:
-                    print("No matches found!")
+                    out_f.write("No matches found!\n")
 for file in os.listdir():
     # Check whether file is in text format or not
     if file.endswith(".tsv"):
         file_path = f"{path}/{file}"
 
-        read_tsv_file(file_path)
+        read_tsv_file(file_path, args.output)
--- scripts/test-1.nf
+++ scripts/test-1.nf
@@ -1,60 +1,49 @@
-params.seqs = "/home/nikoverg/my_interproscan/interproscan-5.63-95.0/test_proteins_redundant.fasta"
-params.separated_seqs = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/seperated_secs/"
-params.seqs_for_interpro = '/home/nikoverg/Documents/bioinformatics/projects/Thesis/seperated_secs/Sequence_*.fasta'
-params.outdir = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/interproscan_results/"
+params.seqs = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/separated_secs/separated_secs.test1"
+params.separated_seqs = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/separated_secs/separated_secs.fasta"
+params.interproscan_dir = "/home/nikoverg/my_interproscan/interproscan-5.63-95.0/"
+params.outdir = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/results"
 params.outdir_results = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/interproscan_results/"
-params.outdir_tsv = "/home/nikoverg/Documents/bioinformatics/projects/Thesis/interproscan_results/Sequence_*.tsv"
 
 
-process Seq_Sep {
-    publishDir params.separated_seqs, mode:'copy'
-    input :
-    path seqs
 
-    output :
-    path 'Sequence_*.fasta'
 
-    script:
-    """
-    Seqs_Separator.py  
-    """
-}
 
 process INTERPROSCAN {
-    publishDir params.outdir_results, mode:'copy'
+    publishDir "${params.outDir}annotations/interproscan", mode: "copy" label "ips"
+
     input :
     path input_seq
 
     output :
-    path "$input_seq"
+    path "${input_seq}.tsv"
     
     script:
     """
-    interproscan.sh -i ${input_seq} -b ${params.outdir_results}
+    ${params.interproscan_dir}interproscan.sh -dp -i ${input_seq}  -f tsv
     """
 
 }
 
 process FISHERMAN {
 
-    cpus 2
+    
     input :
-    path input_seq
+    path "${input_seq}.tsv"
 
     output :
-    path "match_results.txt"
+    path "${params.outdir}"
 
     script:
     """
-    fisherman.py > match_results.txt
+    fisherman.py -i ${fasta}.tsv -o ${params.outdir}
     """
 }
 
 workflow {
-    Seq_Sep_ch = Seq_Sep(params.seqs)
-    sep_seqs = Seq_Sep_ch.flatMap()
-    interproscan_ch = INTERPROSCAN(sep_seqs)
-    FISHERMAN(interproscan_ch)
-
-
-
+    Channel
+        .fromPath(params.seqs)
+        .splitFasta(by : 10 , file: params.separated_seqs)
+        .set {sep_seqs_ch}
+    INTERPROSCAN(sep_seqs_ch)
+    
+}
